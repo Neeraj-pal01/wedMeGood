bc1a988c9aa3a7c021b8ccf7cbfe39e8393aa524
Neeraj pal <ansupal01@gmail.com>
2025-01-23 13:04:21 +0530

    complete 2nd section best ceremonies

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import ImageSlider from '../imgSlider/ImageSlider'
+import ImageSlider2 from '../imgSlider/ImageSlider2'
 
 export default function Home() {
 
-  
+
 
   return (
     <>
-    <section className='bg-[#e6f5f0] '>
+      <section className='bg-[#e6f5f0] '>
 
-      <div className='w-4/5 mx-auto py-10 flex flex-col gap-10'>
-      <div className='flex flex-col gap-4'>
+        <div className='w-4/5 mx-auto py-10 flex flex-col gap-10'>
+          <div className='flex flex-col gap-5'>
 
-        <h2 className='text-2xl text-gray-600 font-medium'>Popular Vanue Search</h2>
-        <ImageSlider />
-      </div>
-      <div>
+            <h2 className='text-2xl text-gray-600 font-medium'>Popular Vanue Search</h2>
+            <ImageSlider />
+          </div>
+          <div className='flex flex-col gap-5 pt-5'>
+            <h2 className='text-2xl  text-gray-600 font-medium'>Best Ceremonies</h2>
+            <ImageSlider2 />
+          </div>
+        </div>
+      </section>
+      <section className='w-4/5 mx-auto'>
 
-        <h2 className='text-2xl pt-  text-gray-600 font-medium'>Popular Vanue Search</h2>
-      </div>
-      </div>
-    </section>
+      </section>
     </>
   )
 }
---
+++ src/Component/imgSlider/ImageSlider2.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react';
+import img1 from '../../assets/ImageSlider/WeddingFun.jpg';
+import img2 from '../../assets/ImageSlider/Photographs.jpg';
+import img3 from '../../assets/ImageSlider/BridalWear.jpg';
+import img4 from '../../assets/ImageSlider/Invitation.jpg';
+import img5 from '../../assets/ImageSlider/BridalMakup.jpg';
+
+const ImageSlider2 = () => {
+  const initialImgs = [
+    {
+      img: img1,
+      venue: 'Wedding Function',
+    },
+    {
+      img: img2,
+      venue: 'Photographs',
+    },
+    {
+      img: img3,
+      venue: 'BridalWear',
+    },
+    {
+      img: img4,
+      venue: 'Invitations',
+    },
+    {
+      img: img5,
+      venue: 'Bridal Makeup',
+    },
+    
+  ];
+
+  const [imgs, setImgs] = useState(initialImgs);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      slide();
+    }, 3000); // Slide every 3 seconds
+
+    return () => clearInterval(interval);
+  }, [imgs]);
+
+  const slide = () => {
+    const updatedImgs = [...imgs];
+    const firstImg = updatedImgs.shift();
+    updatedImgs.push(firstImg);
+    setImgs(updatedImgs);
+  };
+
+  return (
+  
+      <div className="flex transition-transform ease-in-out duration-500 justify-between" >
+        {imgs.slice(0, 4).map((i, index) => (
+          <div
+            key={index}
+            className=" w-[22%] flex-shrink-0  group" 
+          >
+            <img
+              src={i.img} alt=""
+              className="object-cover h-80 w-full shadow-2xl hover:shadow-sm shadow-black rounded-2xl"
+            />
+            <div className='text-[#e72e77] pt-5 text-center'>
+              <p className=" text-xl font-semibold italic ">{i.venue} </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    
+  );
+};
+
+export default ImageSlider2;
